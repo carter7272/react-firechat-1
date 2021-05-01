0174737ad8ec7f74cf6bd5f53fb684d00d07beb7
carter7272 <83476454+carter7272@users.noreply.github.com>
2021-05-01 11:28:05 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -19,17 +19,6 @@ firebase.initializeApp({
 
 });
 
-const SunIcon = props => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 20 20"
-    fill="currentColor"
-    {...props}
-  >
-    <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z" />
-  </svg>
-);
-
 function App() {
   const { user, initializing } = useAuthState(firebase.auth());
   const [darkMode, setDarkMode] = useDarkMode();
@@ -37,8 +26,6 @@ function App() {
   const brandLogo = darkMode
     ? `${process.env.PUBLIC_URL}/logo_white.svg`
     : `${process.env.PUBLIC_URL}/logo.svg`;
-
-  const ThemeIcon = darkMode ? SunIcon : MoonIcon;
 
   const signInWithGoogle = async () => {
     // Retrieve Google provider object
