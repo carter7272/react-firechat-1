ba49236e5a6c9a24732fc666d3b0888c327e6a4b
carter7272 <83476454+carter7272@users.noreply.github.com>
2021-05-01 10:51:38 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,12 +10,13 @@ import Channel from './components/Channel';
 import Loader from './components/Loader';
 
 firebase.initializeApp({
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_AUTH_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
+  apiKey: "AIzaSyCFaXJClEaUbZBLkxfwOTP73lplpSF5phs",
+  authDomain: "react-firechat-88e36.firebaseapp.com",
+  projectId: "react-firechat-88e36",
+  storageBucket: "react-firechat-88e36.appspot.com",
+  messagingSenderId: "192285325096",
+  appId: "1:192285325096:web:e1e6528524ebf3a6a7421d",
+
 });
 
 const MoonIcon = props => (
