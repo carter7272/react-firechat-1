8f59a8739792d75b67ec750307464b77b77a304e
carter7272 <83476454+carter7272@users.noreply.github.com>
2021-05-01 11:21:45 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -19,12 +19,7 @@ firebase.initializeApp({
 
 });
 
-const MoonIcon = props => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 20 20"
-    fill="currentColor"
-    {...props}
+
   >
     <path
       fillRule="evenodd"
