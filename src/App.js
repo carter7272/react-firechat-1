2ae3d7cb38fe64f14bebe017ae0c44df0b24f72c
carter7272 <83476454+carter7272@users.noreply.github.com>
2021-05-01 11:33:00 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -18,6 +18,32 @@ firebase.initializeApp({
   appId: "1:192285325096:web:e1e6528524ebf3a6a7421d",
 
 });
+const MoonIcon = props => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 20 20"
+    fill="currentColor"
+    {...props}
+  >
+    <path
+      fillRule="evenodd"
+      d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
+      clipRule="evenodd"
+    />
+  </svg>
+);
+
+const SunIcon = props => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 20 20"
+    fill="currentColor"
+    {...props}
+  >
+    <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z" />
+  </svg>
+);
+
 
 function App() {
   const { user, initializing } = useAuthState(firebase.auth());
@@ -26,7 +52,7 @@ function App() {
   const brandLogo = darkMode
     ? `${process.env.PUBLIC_URL}/logo_white.svg`
     : `${process.env.PUBLIC_URL}/logo.svg`;
-
+  const ThemeIcon = darkMode ? SunIcon : MoonIcon;
   const signInWithGoogle = async () => {
     // Retrieve Google provider object
     const provider = new firebase.auth.GoogleAuthProvider();
@@ -123,7 +149,7 @@ function App() {
         className="flex-shrink-0 flex items-center justify-between px-4 sm:px-8 shadow-md"
         style={{ height: 'var(--topbar-height)' }}
       >
-        <a href="https://alterclass.io/courses/react">
+        <a href="">
           <img src={brandLogo} alt="AlterClass" width={150} />
         </a>
         <div className="flex items-center">
@@ -135,6 +161,10 @@ function App() {
               Sign out
             </button>
           ) : null}
+          <ThemeIcon
+            className="h-8 w-8 cursor-pointer"
+            onClick={() => setDarkMode(prev => !prev)}
+          />
         </div>
       </header>
       <main
